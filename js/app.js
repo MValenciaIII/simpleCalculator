e587ec2e38c85d9ba4e5827cb55c73c94614a52a
Margarito Valencia <margaritovalenciaIII@gmail.com>
2019-11-14 16:06:19 -0600

    Update 1

---
+++ js/app.js
@@ -0,0 +1,28 @@
+/*
+ If a number is pressed, it will display on the input text box (calculator screen).
+
+*/
+
+//grab the id of everything we need.
+var button = document.querySelectorAll('.calculator button');
+
+
+// button.addEventListener('click', function () {
+//     //do stuff
+//     if(button) {
+//         //grab the value of the button that was clicked.
+        
+//     }
+// })
+
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', function () {
+            //do stuff
+            if(button) {
+                //grab the value of the button that was clicked.
+                var addScreen = document.getElementById('screen')
+                addScreen.innerText = i;
+            }
+        })
+    
+}
