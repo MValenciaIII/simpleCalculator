b6e27c2a4372914c5868855bb11818dc2d389193
Margarito Valencia <margaritovalenciaIII@gmail.com>
2019-11-15 14:23:35 -0600

    update 2

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@
 */
 
 //grab the id of everything we need.
-var button = document.querySelectorAll('.num');
+var button = document.querySelectorAll('.calculator .num');
+var buttonValue = document.querySelectorAll('.calculator .num').value;
 
 
 // button.addEventListener('click', function () {
@@ -20,9 +21,20 @@ for (let i = 0; i < button.length; i++) {
             //do stuff
             if(button) {
                 //grab the value of the button that was clicked.
+                
                 var addScreen = document.getElementById('screen')
-                addScreen.innerText = 9 - i;
+                addScreen.innerText = event.target.getAttribute('value');
             }
+            if()
         })
     
 }
+
+// button.addEventListener('click', function () {
+//     //             //do stuff
+//                 if(button) {// add when there is a value add another number.
+//                     //grab the value of the button that was clicked.
+//                     var addScreen = document.getElementById('screen')
+                    
+//                 }
+//             })
